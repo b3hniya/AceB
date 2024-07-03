356329d6d1925a5d20488a000d33a682150c1a53
Behniya <b3hniya@gmail.com>
2024-07-03 12:28:22 +0330

    Fix: refactor dyanmicRouter.ts

--- src/middlewares/dynamicRouter.ts
+++ src/middlewares/dynamicRouter.ts
@@ -2,8 +2,7 @@ import fs from "fs";
 import path from "path";
 import { Router } from "express";
 
-const toKebabCase = (str: string) =>
-  str.replace(/([a-z])([A-Z])/g, "$1-$2").toLowerCase();
+const toKebabCase = (str: string) => str.replace(/([a-z])([A-Z])/g, "$1-$2").toLowerCase();
 
 /**
  * Dynamically loads controllers from a given directory and attaches them to a router.
@@ -13,6 +12,8 @@ const toKebabCase = (str: string) =>
  * const router = dynamicRouter(path.join(__dirname, "controllers/"));
  * app.use("/api", router);
  */
+const MODE = process.env.MODE || "DEV";
+
 const dynamicRouter = (baseDir: string): Router => {
   const router = Router();
 
@@ -27,16 +28,30 @@ const dynamicRouter = (baseDir: string): Router => {
       } else if (item.endsWith("Controller.ts")) {
         const controller = require(fullPath);
 
-        const baseRoute = `/${
-          relativePath ? relativePath + "/" : ""
-        }${toKebabCase(item.replace("Controller.ts", ""))}`;
-        console.log(`baseRoute: ${baseRoute}`); // Log the baseRoute for debugging
+        const baseRoute = `/${relativePath ? relativePath + "/" : ""}${toKebabCase(item.replace("Controller.ts", ""))}`;
 
         if (controller.put) router.put(baseRoute, controller.put);
         if (controller.get) router.get(baseRoute, controller.get);
         if (controller.post) router.post(baseRoute, controller.post);
         if (controller.patch) router.patch(baseRoute, controller.patch);
         if (controller.delete) router.delete(baseRoute, controller.delete);
+
+        if (MODE === "DEV") {
+          const methods: string[] = [];
+
+          if (controller.put) methods.push("PUT");
+          if (controller.get) methods.push("GET");
+          if (controller.post) methods.push("POST");
+          if (controller.patch) methods.push("PATCH");
+          if (controller.delete) methods.push("DELETE");
+
+          // Log the baseRoute for debugging
+          if (methods.length > 0) {
+            console.log(`baseRoute: ${baseRoute} with following methods: ${methods.join(", ")}`);
+          } else {
+            console.log(`No methods found for baseRoute: ${baseRoute}`);
+          }
+        }
       }
     });
   };
